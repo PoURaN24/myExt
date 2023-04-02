158721cd6bc396259b5b73610e18a1608bc2eb74
DK\oura <panagiotis.ouranos@sitecore.com>
2023-04-02 03:01:38 +0300

    Add "slack" functionality. Check if the url is currently slack.   > If not redirect AND when finished loading read cookie/localstorage and alert them.   > if already in slack just alert cookie/LStorage again.

--- script.js
+++ script.js
@@ -10,50 +10,59 @@ chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
 // "aa".split("").reduce((hex,c)=>hex+=c.charCodeAt(0).toString(16).padStart(4,"0"),"")
 
 
+
 /////////////////////////////////////////////////////////
 // get a cookie from the active tab, with name "d"
-chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-  // Get the current tab
-  var activeTab = tabs[0];
+function getSlackCookie() {
+	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+	  // Get the current tab
+	  var activeTab = tabs[0];
 
-  // Get the cookie by name
-  chrome.cookies.get({ url: activeTab.url, name: 'd' }, function (cookie) {
-      debugger;
-    if (cookie) {
-      console.log(cookie.value);
-    } else {
-      console.log('Cookie not found!');
-    }
-  });
-});
-
+	  // Get the cookie by name
+	  chrome.cookies.get({ url: activeTab.url, name: 'd' }, function (cookie) {
+		  //debugger;
+		if (cookie) {
+		  alert(cookie.value);
+		} else {
+		  console.log('Cookie not found!');
+		}
+	  });
+	});
+}
 
 
 
 ////////////////////////////////////////////////////////////////////////////////////
 // get and parse a value from localStorage from the active tab, with name "localConfig_v2"
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  chrome.scripting.executeScript({
-    target: {tabId: tabs[0].id},
-    function: function() {
-      var myVariable = JSON.parse(localStorage.getItem('localConfig_v2'));
-      return myVariable.teams.T1S2RKGUA.token;
-    }
-  }, function(result) {
-    if (chrome.runtime.lastError) {
-      console.error(chrome.runtime.lastError);
-    } else {
-      console.log(result[0].result);
-    }
-  });
-});
+function getSlacklocal() {
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	  chrome.scripting.executeScript({
+		target: {tabId: tabs[0].id},
+		function: function() {
+		  var myVariable = JSON.parse(localStorage.getItem('localConfig_v2'));
+		  return myVariable.teams.T1S2RKGUA.token;
+		}
+	  }, function(result) {
+		if (chrome.runtime.lastError) {
+		  console.error(chrome.runtime.lastError);
+		} else {
+		  //console.log(result[0].result);
+		  alert(result[0].result);
+		}
+	  });
+	});
+}
 
 
-
+//////// ADDING CLICK HANDLERS
 document.getElementById('youtube').addEventListener('click', yt);
-document.getElementById('general').addEventListener('click', gen);
+document.getElementById('general').addEventListener('click', slackStuff);
 
-function gen() {
+
+
+
+// handler of generic button
+function slackStuff() {
 	var mode;
 
 	if(document.getElementById('general').style.color == 'gray') // an einai gray einai off..
@@ -61,11 +70,37 @@ function gen() {
 	else
 		mode = 1;
 
-	if(!mode)
+	if(!mode) {
 		document.getElementById('general').style.color = 'blue';
-	else
+		mode = 1;
+	}
+	else {
 		document.getElementById('general').style.color = 'gray';
+		mode = 0;
+	}
 
+	if(mode) {	// if enabled, do this:
+		chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+			var url = tabs[0].url;
+			console.log("Checking URL of the active tab : " + url);
+			if(url.indexOf('slack') >= 0) {	// if you are in slack already
+				getSlackCookie();
+				getSlacklocal();
+			} else {	// else if you are not, redirect there..
+				console.log('trying to redirect to slack..');
+				chrome.tabs.update({url: "https://app.slack.com/client/T1S2RKGUA/C1S26F0F8"}, function(tab) {
+					chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+						if (tabId === tab.id && changeInfo.status === 'complete') {
+						  // call your function here
+						  console.log('finished loading...!');
+						  getSlackCookie();
+						  getSlacklocal();
+						}
+					})
+				});
+			}
+		});
+	}
 }
 
 function yt() {
